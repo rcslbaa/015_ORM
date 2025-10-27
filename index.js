51a134f844045e9119ca567adbb96065785b07eb
ifa asmarani rosalba <160864416+rcslbaa@users.noreply.github.com>
2025-10-27 13:01:36 +0700

    menambahkan app get

--- index.js
+++ index.js
@@ -32,3 +32,12 @@ app.post("/komik", async (req, res) => {
         res.send(err);
     }
 })
+
+app.get("/komik", async (req, res) => {
+    try{
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (err){
+        res.send(err);
+    }
+})
