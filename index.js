2edca9405411dd8cfcdba8a4cf77caee88e19b1d
ifa asmarani rosalba <160864416+rcslbaa@users.noreply.github.com>
2025-10-27 12:59:42 +0700

    Update index.js
    Refactor server initialization and add POST endpoint for 'komik'.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 const db = require('./models');
 app.use(express.json());
 app.use(
@@ -13,12 +13,22 @@ app.listen(PORT, () => {
     console.log(`Server started on port 3000`);
 });
 
-db.sequelize.sync().then((result) => {
-    app
-        .listen(3000, () => {
-            console.log("Server started");
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+                console.log("Server started");
+            })
         })
         .catch((err) => {
             console.log(err);
         });
-});
+
+app.post("/komik", async (req, res) => {
+    const data = req.body;
+    try{
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (err){
+        res.send(err);
+    }
+})
